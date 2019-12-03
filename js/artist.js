f7b05350410a92c3ed95d85a8192f8e144cadac6
Emilio <a01651283@itesm.mx>
2019-12-03 14:33:59 -0600

    Front end added

---
+++ js/artist.js
@@ -0,0 +1,129 @@
+var token = localStorage.getItem('token');
+if (token) {
+  token = token.replace(/^"(.*)"$/, '$1'); // Remove quotes from token start/end.
+}
+
+
+var artists = document.querySelectorAll("input[type=checkbox]");
+
+function loadArtists() {
+  $.ajax({
+    url: 'https://topfy-back.herokuapp.com/artists',
+    // url: 'https://tuapp.herokuapp.com/todos',
+    headers: {
+        'Content-Type':'application/json',
+        'Authorization': 'Bearer ' + token
+    },
+    method: 'GET',
+    dataType: 'json',
+    success: function(data){
+      console.log(data)
+
+      for( let i = 0; i < data.length; i++) {
+        // aqui va su código para agregar los elementos de la lista
+        console.log(data[i].name)
+        // algo asi:
+        // addTodo(data[i]._id, data[i].description, data[i].completed)
+        // no tienen que usar la funcion de addTodo, es un ejemplo
+        $('#todo-list').append(`<li><input type="checkbox" name="todo" value="2"><span>${data[i].name}</span></li>`)
+      }
+    },
+    error: function(error_msg) {
+      alert((error_msg['responseText']));
+    }
+  });
+}
+
+loadArtists()
+
+
+// o con jquery
+// $('input[name=newitem]').keypress(function(event){
+//     var keycode = (event.keyCode ? event.keyCode : event.which);
+//     if(keycode == '13'){
+//         $.ajax({})
+//     }
+// });
+
+var input = document.querySelector("input[name=newitem]");
+
+input.addEventListener('keypress', function (event) {
+  if (event.charCode === 13) {
+    json_to_send = {
+      "name" : input.value
+    };
+    json_to_send = JSON.stringify(json_to_send);
+    $.ajax({
+      url: 'https://topfy-back.herokuapp.com/artists',
+      // url: 'https://tuapp.herokuapp.com/todos',
+      headers: {
+          'Content-Type':'application/json',
+          'Authorization': 'Bearer ' + token
+      },
+      method: 'POST',
+      dataType: 'json',
+      data: json_to_send,
+      success: function(data){
+        console.log(data)
+        // agregar código aqui para poner los datos del todolist en el el html
+        $('#todo-list').append(`<li><input type="checkbox" name="todo" value="2"><span>${input.value}</span></li>`)
+        //input.value = "";
+      },
+      error: function(error_msg) {
+        alert((error_msg['responseText']));
+      }
+    });
+  }
+})
+
+
+function addTodo(id, todoText, completed) {
+  
+}
+
+$("#logout-button").on("click", function(){
+  localStorage.removeItem('token');
+  window.location = './index.html'
+})
+
+$('#create-playlist').on('click', function(){
+  var artistString = ""
+  $.ajax({
+    url: 'https://topfy-back.herokuapp.com/artists',
+    // url: 'https://tuapp.herokuapp.com/todos',
+    headers: {
+        'Content-Type':'application/json',
+        'Authorization': 'Bearer ' + token
+    },
+    method: 'GET',
+    dataType: 'json',
+    success: function(data){
+      console.log(data)
+
+      for(let i = 0; i < data.length; i++) {
+        // aqui va su código para agregar los elementos de la lista
+        console.log(data[i].name)
+        // algo asi:
+        // addTodo(data[i]._id, data[i].description, data[i].completed)
+        // no tienen que usar la funcion de addTodo, es un ejemplo
+
+        artistString += data[i].name + ",";
+      }
+
+      if(artistString.length != 0){
+        artistString = artistString.substring(0, artistString.length-1)
+      }
+
+
+      //var spotifyLink = "https://spotify-back-project.herokuapp.com/spotify?artists=" + artistString
+      window.location = "https://spotify-back-project.herokuapp.com/spotify?artists=" + artistString
+
+    },
+    error: function(error_msg) {
+      alert((error_msg['responseText']));
+    }
+  });
+
+
+
+})
